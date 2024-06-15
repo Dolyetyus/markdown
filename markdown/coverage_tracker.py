48d0bffe4bce895dc8f159e2d57cc82f58c6561d
Doli <67073431+Dolyetyus@users.noreply.github.com>
2024-06-15 03:03:14 +0200

    Add files via upload

---
+++ markdown/coverage_tracker.py
@@ -0,0 +1,25 @@
+# coverage_tracker.py
+branch_coverage_hl = {
+    "parse_hl_lines_10": False,
+    "parse_hl_lines_11": False,
+    "parse_hl_lines_12": False
+}
+
+branch_coverage_cd = {
+    "hilite_13": False,
+    "hilite_14": False,
+    "hilite_15": False,
+    "hilite_16": False,
+    "hilite_17": False,
+    "hilite_18": False,
+    "hilite_19": False,
+    "hilite_21": False,
+    "hilite_22": False,
+    "hilite_23": False,
+    "hilite_24": False,
+    "hilite_25": False,
+    "hilite_26": False,
+    "hilite_27": False,
+    "hilite_28": False,
+    "hilite_29": False
+}
