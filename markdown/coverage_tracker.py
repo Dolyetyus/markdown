04c1c0650cd7bcb0e35f8d1aca1b7b120ee822bd
Doli <67073431+Dolyetyus@users.noreply.github.com>
2024-06-27 09:58:22 +0200

    Manually solved merge conflicts

--- markdown/coverage_tracker.py
+++ markdown/coverage_tracker.py
@@ -1,9 +1,39 @@
 # coverage_tracker.py
 
-branch_coverage_hiliteInit = {
-    "codehilite_init_40": False,
-    "codehilite_init_41": False,
-    "codehilite_init_42": False
+branch_coverage_HT = {
+    "run_1": False,
+    "run_2": False,
+    "run_3": False 
+}
+
+branch_coverage_html_2_text = {
+    "html_2_text_1": False,
+    "html_2_text_2": False
+}
+
+branch_coverage_hl = {
+    "parse_hl_lines_10": False,
+    "parse_hl_lines_11": False,
+    "parse_hl_lines_12": False
+}
+
+branch_coverage_cd = {
+    "hilite_13": False,
+    "hilite_14": False,
+    "hilite_15": False,
+    "hilite_16": False,
+    "hilite_17": False,
+    "hilite_18": False,
+    "hilite_19": False,
+    "hilite_21": False,
+    "hilite_22": False,
+    "hilite_23": False,
+    "hilite_24": False,
+    "hilite_25": False,
+    "hilite_26": False,
+    "hilite_27": False,
+    "hilite_28": False,
+    "hilite_29": False
 }
 
 branch_coverage_dequote = {
@@ -19,20 +49,15 @@ branch_coverage_parse_header = {
     "parse_header_36": False
 }
 
+branch_coverage_hiliteInit = {
+    "codehilite_init_40": False,
+    "codehilite_init_41": False,
+    "codehilite_init_42": False
+}
+
 branch_coverage_hiliteExtInit = {
     "codehilite_initExt_43": False,
     "codehilite_initExt_44": False,
     "codehilite_initExt_45": False,
     "codehilite_initExt_46": False
-}
-
-branch_coverage_HT = {
-    "run_1": False,
-    "run_2": False,
-    "run_3": False 
-}
-
-branch_coverage_html_2_text = {
-    "html_2_text_1": False,
-    "html_2_text_2": False
 }
